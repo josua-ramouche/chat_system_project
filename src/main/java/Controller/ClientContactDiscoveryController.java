525827c3ca0b362a65d73d767bcc953d045d6531
Josua <josua.ramouche@live.fr>
2023-10-27 14:36:31 +0200

    Client/Server Contact Discovery ...

--- src/main/java/Controller/ClientContactDiscoveryController.java
+++ src/main/java/Controller/ClientContactDiscoveryController.java
@@ -1,9 +1,16 @@
 package Controller;
 
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Objects;
+
 public class ClientContactDiscoveryController {
         private static DatagramSocket socket = null;
 
-        public static void main((String[] args)) throws IOException {
+        public static void main((String[] args)) throws void IOException {
             broadcast("Hello", InetAddress.getByName("255.255.255.255"));
         }
 
@@ -18,5 +25,24 @@ public class ClientContactDiscoveryController {
                     = new DatagramPacket(buffer, buffer.length, address, 4445);
             socket.send(packet);
             socket.close();
+        }
+
+        List<InetAddress> listAllBroadcastAddresses() throws SocketException {
+            List<InetAddress> broadcastList = new ArrayList<>();
+            Enumeration<NetworkInterface> interfaces
+                    = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = interfaces.nextElement();
+
+                if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+                    continue;
+                }
+
+                networkInterface.getInterfaceAddresses().stream()
+                        .map(InterfaceAddress::getBroadcast)
+                        .filter(Objects::nonNull)
+                        .forEach(broadcastList::add);
+            }
+            return broadcastList;
         }
 }
--- src/main/java/Controller/ServerContactDiscoveryController.java
+++ src/main/java/Controller/ServerContactDiscoveryController.java
@@ -7,24 +7,7 @@ import java.util.Objects;
 
 
 public class ServerContactDiscoveryController {
-    List<InetAddress> listAllBroadcastAddresses() throws SocketException {
-        List<InetAddress> broadcastList = new ArrayList<>();
-        Enumeration<NetworkInterface> interfaces
-                = NetworkInterface.getNetworkInterfaces();
-        while (interfaces.hasMoreElements()) {
-            NetworkInterface networkInterface = interfaces.nextElement();
 
-            if (networkInterface.isLoopback() || !networkInterface.isUp()) {
-                continue;
-            }
-
-            networkInterface.getInterfaceAddresses().stream()
-                    .map(InterfaceAddress::getBroadcast)
-                    .filter(Objects::nonNull)
-                    .forEach(broadcastList::add);
-        }
-        return broadcastList;
-    }
 
 
 
