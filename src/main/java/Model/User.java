3ac2f9f90b4c71acd0ea194a295c2434a523120b
SaladeVerteee <cossoul.lucile@gmail.com>
2023-11-10 11:54:23 +0100

    user update

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,9 +1,14 @@
 package Model;
+import java.net.InetAddress;
 import java.util.List;
 public class User {
 
 
     private String username;
+
+    private InetAddress ipaddress;
+
+    public Boolean state;
 
     // Contact List updated after connection, and each time another user connects
     private List<User> ContactList;
