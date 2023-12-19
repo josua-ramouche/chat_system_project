ee1c24056bdd81f1e78ff9f17d6a9481d2c8c0db
Josua <josua.ramouche@live.fr>
2023-12-19 18:06:53 +0100

    DatabaseController creation

--- src/main/java/Controller/DatabaseController.java
+++ src/main/java/Controller/DatabaseController.java
@@ -4,14 +4,15 @@ package Controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseController {
 
-    public static void connect() {
+    public static Connection connect() {
         Connection conn = null;
         try {
             //Database parameters
-            String url = "jdbc:sqlite:sample.db";
+            String url = "jdbc:sqlite:chatsystem.db";
             //Create a connection to the database
             conn = DriverManager.getConnection(url);
 
@@ -19,19 +20,96 @@ public class DatabaseController {
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
+        }
+        return conn;
+    }
+
+
+    // Create a User Table in database
+    public static void createUserTable() {
+        Connection conn = connect();
+        // Create User table after connection to the database
+        try (Statement stmt = conn.createStatement()) {
+            String sql = "CREATE TABLE IF NOT EXISTS Users (\n"
+                    + "		userID INTEGER PRIMARY KEY AUTOINCREMENT, \n"
+                    + "		username TEXT NOT NULL, \n"
+                    + "     ipaddress TEXT NOT NULL, \n"
+                    + "     connectionState BOOLEAN NOT NULL \n"
+                    + ");";
+            stmt.executeUpdate(sql);
+            System.out.println("User table created successfully\n");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // End the connection after the creation of the table
+        finally {
             try {
-                if(conn != null) {
+                if (conn != null) {
                     conn.close();
                 }
-            }  catch (SQLException e) {
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    //Create a Contact Table in database
+    public static void createContactTable() {
+        Connection conn = connect();
+        // Create Contact table after connection to the database
+        try (Statement stmt = conn.createStatement()) {
+            String sql = "CREATE TABLE IF NOT EXISTS Contacts (\n"
+                    + "		contactID INTEGER PRIMARY KEY AUTOINCREMENT, \n"
+                    + "		chatID UNIQUE INTEGER AUTOINCREMENT \n"
+                    + ");";
+            stmt.executeUpdate(sql);
+            System.out.println("Contacts table created successfully\n");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // End the connection after the creation of the table
+        finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    //Create a Chat Table in the database
+    public static void createChatTable(int id) {
+        Connection conn = connect();
+        // Create Contact table after connection to the database
+        try (Statement stmt = conn.createStatement()) {
+            String sql = "CREATE TABLE IF NOT EXISTS Chat" + id + "(\n"
+                    + "		messageID INTEGER PRIMARY KEY AUTOINCREMENT, \n"
+                    + "		message TEXT NOT NULL, \n"
+                    + "     time DATETIME NOT NULL \n"
+                    + ");";
+            stmt.executeUpdate(sql);
+            System.out.println("Chat table created successfully\n");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // End the connection after the creation of the table
+        finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
     public static void main(String[] args) {
-        connect();
+        createUserTable();
+        createContactTable();
+        createChatTable(1);
     }
 }
 
