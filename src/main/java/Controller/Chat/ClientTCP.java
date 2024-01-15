86e88bd42d492e8a070ee8e3c5d5b4a0120df764
SaladeVerteee <cossoul.lucile@gmail.com>
2024-01-15 15:58:26 +0100

    TCP with more than 2 users

--- src/main/java/Controller/Chat/ClientTCP.java
+++ src/main/java/Controller/Chat/ClientTCP.java
@@ -6,17 +6,25 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClientTCP {
-    static Socket socket;
+    private static Map<InetAddress, Socket> socketMap = new HashMap<>();
     private static PrintWriter out;
     private static BufferedReader in;
 
+    public static Map<InetAddress, Socket> getMap() {
+        return socketMap;
+    }
+
     public static void startConnection(InetAddress ip, int port) {
         try {
-            socket = new Socket(ip, port);
+            Socket socket = new Socket(ip, port);
+            socketMap.put(ip,socket);
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            socketMap.put(ip, socket); // Store the socket in the map
             System.out.println("Welcome to the ChatSystem client\n");
         } catch (IOException e) {
             e.printStackTrace();
@@ -24,18 +32,19 @@ public class ClientTCP {
     }
 
     public static void sendMessage(String message) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            out.println(message);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        out.println(message);
     }
 
     public static void stopConnection() {
         try {
             in.close();
             out.close();
-            socket.close();
+            for (Socket socket : socketMap.values()) {
+                socket.close();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
